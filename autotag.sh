34595220d55fce96b710c3752983fbe905f03ecf
lvzk <897706680@qq.com>
2023-02-10 10:20:52 +0800

    init

--- autotag.sh
+++ autotag.sh
@@ -1,7 +1,16 @@
+#Copyright(c) $today.year NeatLogic Co., Ltd. All Rights Reserved.
 #
-# Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
-# 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
 #
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
 
 POM_VERSION=`cat pom.xml|grep "<version>"|sed -n '1p'`
 POM_VERSION=${POM_VERSION#*>}
